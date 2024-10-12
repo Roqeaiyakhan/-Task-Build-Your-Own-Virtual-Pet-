406e1122edf4d043aa27deb2f1f832dc36faf0ba
Roqeaiyakhan <roqaiyajaani@gmail.com>
2024-10-12 16:10:06 +0500

    add  feedding method

---
+++ script.js
@@ -0,0 +1,33 @@
+let pet = {
+    name: "puppy",
+    type: "dog",
+    age: 3,
+   happiness: 66,
+   hunger: 70,
+
+//    feeding method
+    feed: function(){
+    this.hunger -= 20;
+    if (this.hunger < 0) {
+        this.hunger = 0;
+    }
+    alert(`${this.name} has been feed! hunger level is now ${this.hunger}.`)
+
+    },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
